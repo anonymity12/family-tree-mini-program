ea461266b6b730bc312f9e1f5b5b457b0518dae1
paulxcode <paulblue97@yahoo.com>
2025-01-27 17:34:51 +0800

    家谱start

---
+++ miniprogram/pages/family-tree/index.js
@@ -0,0 +1,18 @@
+// pages/family-tree/index.js
+Page({
+  data: {
+    // 初始数据
+  },
+
+  onLoad: function(options) {
+    // 页面加载时执行
+  },
+
+  onReady: function() {
+    // 页面初次渲染完成时执行
+  },
+
+  onShow: function() {
+    // 页面显示时执行
+  }
+})
